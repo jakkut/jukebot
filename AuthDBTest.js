04e3f855365fb3643b64e49c8e5a1bd5f9e6ce88
joysv <joysv@umich.edu>
2025-02-26 18:35:47 -0500

    Firebase Authentication/Database notes

---
+++ AuthDBTest.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+/* 
+FireBase free tier
+ -Free Authentication with limited users
+ -1 GB stored data
+ -50,000 reads, 50,000 writes and 50,000 deletes per day
+ -Hosting: 10 GB storage and 360 MB data transfer per day
+ -Cloud Functions: 2 million invocations per month
+*/
+
+/* FROM FIREBASE CONSOLE WEBPAGE
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDf6-YDL4aDgE-oP1dz6X1RF0PyXgFshHU",
+  authDomain: "jukebot-9d46a.firebaseapp.com",
+  projectId: "jukebot-9d46a",
+  storageBucket: "jukebot-9d46a.firebasestorage.app",
+  messagingSenderId: "311579722119",
+  appId: "1:311579722119:web:da8139800a2443e6905322",
+  measurementId: "G-XXFJHLPZ32"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+*/
+
